09beca882fee9b5b5762859800d50b43c3722c76
Cheong <xxlsxx1955@gmail.com>
2022-08-10 18:13:31 +0800

    Refactored code

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-// Objective: 
-// show user prompt
-// collect and standardize user's input
-// return input
-function getPlayerChoise(){
-    let promptText = "Enter your choise: \n(rock,paper,scissors)";
-    let userInput = prompt(promptText);
-    userInput = userInput.toLowerCase();
-    return userInput;
-}
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const rock_button = document.getElementById("rock-button");
+const paper_button = document.getElementById("paper-button");
+const scissors_button = document.getElementById("scissors-button");
+const resultDiv = document.getElementById("result");
 
-// computer choose choise randomly via rng
-function getComputerChoise(){
-    let rng = Math.random();
-    let computerInput = "";
-    (rng<0.33)? computerInput = "rock" :
-    (rng<0.66)? computerInput = "paper" :
-    computerInput = "scissor";
-    return computerInput;
-}
+let isPlaying = true;
 
-// Objective: play a single round
-// create a global variable that store user input and computer choise
 let userInput;
 let computerInput;
-
-// result for single round
 let result;
 
-// possible outcome text
+let userScore = 0;
+let compScore = 0;
+
 const rockRock = "Tie game!";
 const rockPaper = "You lose! Paper beat rock!";
 const rockScissors = "You win! Rock beat scissors!";
@@ -38,66 +24,57 @@ const scissorsRock = "You lose! Rock beat scissors!";
 const scissorsPaper = "You win! Scissors beat paper!";
 const scissorsScissors = "Tie game!";
 
-// pass user and computer input as argument
+const userWin = "User is the winner!";
+const compWin = "Computer is the winner!";
+const tieGame = "There is no winner!";
+
+function getComputerChoise(){
+    let rng = Math.random();
+    let computerInput = "";
+    (rng<0.33)? computerInput = "rock" :
+    (rng<0.66)? computerInput = "paper" :
+    computerInput = "scissor";
+    return computerInput;
+}
+
 function playRound(userInput,computerInput){
 
-    // decide outcome & return result
-    if( userInput == "rock" ){
-        (computerInput == "rock")? result = rockRock :
-        (computerInput == "paper")? result = rockPaper :
-        result = rockScissors;
-    }else if( userInput == "paper" ){
-        (computerInput == "rock")? result = paperRock :
-        (computerInput == "paper")? result = paperPaper :
-        result = paperScissors;
-    }else if( userInput == "scissors" ){
-        (computerInput == "rock")? result = scissorsRock :
-        (computerInput == "paper")? result = scissorsPaper :
-        result = scissorsScissors;
-    }else{
-        alert("Invalid input. Please refer to UI once again.");
+    if( userInput === "rock" ){
+        (computerInput === "rock")? resultDiv.textContent = rockRock :
+        (computerInput === "paper")? resultDiv.textContent = rockPaper :
+        resultDiv.textContent = rockScissors;
+    }else if( userInput === "paper" ){
+        (computerInput === "rock")? resultDiv.textContent = paperRock :
+        (computerInput === "paper")? resultDiv.textContent = paperPaper :
+        resultDiv.textContent = paperScissors;
+    }else if( userInput === "scissors" ){
+        (computerInput === "rock")? resultDiv.textContent = scissorsRock :
+        (computerInput === "paper")? resultDiv.textContent = scissorsPaper :
+        resultDiv.textContent = scissorsScissors;
     }
 
-    // show the result
-    console.log(result);
-    return result;
-}
-
-// Objective: code a function that contain following functionality
-
-// Constraints:
-// keep score for user and computer
-let userScore = 0;
-let computerScore = 0;
-
-function game(){
-    // call playRound function 5 times
-    for(let i=0; i<5; i++){
-        
-        // get user input and computer input through function
-        userInput = getPlayerChoise();
-        computerInput = getComputerChoise();
-        
-        // store result to result variable
-        let result = playRound(userInput,computerInput);
-
-        // calculate user and computer score
-        if(result == rockScissors || result == paperRock || result == scissorsPaper){
-            userScore++;
-        }else if(result == rockPaper || result == paperScissors || result == scissorsRock){
-            computerScore++;
-        }
+    if(resultDiv.textContent === rockScissors || resultDiv.textContent === paperRock || resultDiv.textContent === scissorsPaper){
+        userScore++;
+        playerScore.textContent = userScore;
+    }else if(resultDiv.textContent === rockPaper || resultDiv.textContent === paperScissors || resultDiv.textContent === scissorsRock){
+        compScore++;
+        computerScore.textContent = compScore;
     }
-    
-    // final result text
-    const userWin = "User is the winner!";
-    const compWin = "Computer is the winner!";
-    const tieGame = "There is no winner!";
 
-    // display result via console
-    (userScore > computerScore)? console.log(userWin) :
-    (userScore == computerScore)? console.log(tieGame) :
-    console.log(compWin);
+    if(userScore === 5 || compScore === 5){
+        isPlaying = false;
+        (userScore > compScore)? resultDiv.textContent = userWin :
+        (userScore === compScore)? resultDiv.textContent = tieGame :
+        resultDiv.textContent = compWin;
+    }
 }
 
-game();
+rock_button.addEventListener('click', ()=>{
+    isPlaying && playRound('rock',getComputerChoise());
+});
+paper_button.addEventListener('click', ()=>{
+    isPlaying && playRound('paper',getComputerChoise());
+});
+scissors_button.addEventListener('click', ()=>{
+    isPlaying && playRound('scissors',getComputerChoise());
+});
